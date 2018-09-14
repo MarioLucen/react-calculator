c39d289356ab1bd5e55fa445b41ec879ae634ea2
MarioLucen <mlucena@islautopia.com>
2018-09-14 13:32:24 +0200

    Screen refact

--- app/js/components/Screen.js
+++ app/js/components/Screen.js
@@ -1,15 +1,19 @@
 
 import React, { Component } from 'react'
 
-class Screen extends Component {
-    render() {
-        console.log(this)
-        return (
-            <div className="row">
-                <div className="screen">{this.props.result}</div>
-            </div>
-        )
-    }
-}
+// class Screen extends Component {
+//     render() {
+//         console.log(this)
+//         return (
+//             <div className="row">
+//                 <div className="screen">{this.props.result}</div>
+//             </div>
+//         )
+//     }
+// }
+
+const Screen = (props) => <div className="row">
+    <div className="screen">{props.result}</div>
+</div>
 
 export default Screen
